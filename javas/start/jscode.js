a86e49a87de4693e6a59f20b0fcb958d5e1358bf
maxster256 <132581406+maxster256@users.noreply.github.com>
2024-03-28 16:01:47 +0100

    Rozwiązanie punktu 4 zadania rekrutacyjnego
    Dodanie kolumny dla suwaków,
    zmieniony sposób ustawiania wartości dostosowany do suwaków

--- javas/start/jscode.js
+++ javas/start/jscode.js
@@ -2,10 +2,10 @@
 // fragment kodu zwiazany z sama strona
 
 let table_content = [
-    ["Motor", "Left Control", "Right Control", "Position"],
-    ["Motor 1", "---", "---", "-"],
-    ["Motor 2", "---", "---", "-"],
-    ["Motor 3", "---", "---", "-"]
+    ["Motor", "Left Control", "Right Control", "Control Signal Value", "Position"],
+    ["Motor 1", "ctrl", "ctrl", "slider", "pos"],
+    ["Motor 2", "ctrl", "ctrl", "slider", "pos"],
+    ["Motor 3", "ctrl", "ctrl", "slider", "pos"]
 ]
 
 let buttons = [
@@ -16,9 +16,11 @@ let buttons = [
 
 const rows_number = table_content.length;
 const columns_number = table_content[0].length;
+
+
 let class_name;
 let is_enabled = true;
-let rotation0 = 0, rotation1 = 0, rotation2 = 0;
+let rotation0 = 50, rotation1 = 50, rotation2 = 50;
 
 let grid = document.getElementById("grid");
 
@@ -31,13 +33,29 @@ for (let i = 0; i < rows_number; i++){
 
     for (let j = 0; j < columns_number; j++){
         let cell = document.createElement("td");
-        if(table_content[i][j] == "---"){
+        if(table_content[i][j] == "ctrl"){
             let btn = document.createElement("button");
             btn.textContent = buttons[i-1][j-1];
             btn.addEventListener("click", function(){
                 findButtonOrKey(buttons[i-1][j-1]);
             })
             cell.appendChild(btn);
+        }
+        else if(table_content[i][j] == "slider"){
+            cell.style.fontSize = "11px";
+            let slider = document.createElement("input");
+            slider.type = "range";
+            slider.value = 50;
+            cell.append(slider, "\n", slider.value);
+            slider.addEventListener("input", function(){
+                cell.textContent = "";
+                cell.append(slider, "\n", slider.value);
+                switch(i){
+                    case 1: rotation0 = Number(slider.value); break;
+                    case 2: rotation1 = Number(slider.value); break;
+                    case 3: rotation2 = Number(slider.value); break;
+                }
+            })
         }
         else    {cell.textContent = table_content[i][j];}
         row.appendChild(cell);
@@ -57,7 +75,6 @@ document.getElementById("controlbtn").addEventListener("click", function(){
         label.textContent = "Enabled";
         is_enabled = true;
     }
-
 });
 
 document.addEventListener("keypress", function(){
@@ -67,22 +84,22 @@ document.addEventListener("keypress", function(){
 function findButtonOrKey(btn){
     if(is_enabled){
         switch(btn){
-            case "q": rotation0-=10;
+            case "q":
+                topicPublisher('/virtual_dc_motor_node/set_cs_0', (-1) * rotation0);
+                break;
+            case "e":
                 topicPublisher('/virtual_dc_motor_node/set_cs_0', rotation0);
                 break;
-            case "e": rotation0+=10;
-                topicPublisher('/virtual_dc_motor_node/set_cs_0', rotation0);
+            case "a":
+                topicPublisher('/virtual_dc_motor_node/set_cs_1', (-1) * rotation1);
                 break;
-            case "a": rotation1-=10;
+            case "d":
                 topicPublisher('/virtual_dc_motor_node/set_cs_1', rotation1);
                 break;
-            case "d": rotation1+=10;
-                topicPublisher('/virtual_dc_motor_node/set_cs_1', rotation1);
+            case "z":
+                topicPublisher('/virtual_dc_motor_node/set_cs_2', (-1) * rotation2);
                 break;
-            case "z": rotation2-=10;
-                topicPublisher('/virtual_dc_motor_node/set_cs_2', rotation2);
-                break;
-            case "c": rotation2+=10;
+            case "c":
                 topicPublisher('/virtual_dc_motor_node/set_cs_2', rotation2);
                 break;
             default: break;
@@ -130,7 +147,7 @@ topicListener('/virtual_dc_motor_node/get_position_0', 1);
 topicListener('/virtual_dc_motor_node/get_position_1', 2);
 topicListener('/virtual_dc_motor_node/get_position_2', 3);
 
-function topicPublisher(topic, rot){
+function topicPublisher(topic, rotat){
     var publisher = new ROSLIB.Topic({
         ros : ros,
         name : topic,
@@ -138,7 +155,7 @@ function topicPublisher(topic, rot){
     });
     
     var cs = new ROSLIB.Message({
-        data: rot
+        data: rotat
     });
     publisher.publish(cs);
 }
